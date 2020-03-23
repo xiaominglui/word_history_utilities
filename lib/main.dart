8ff7b547bb135e7e599e6cb86bab8f3a39b08729
xiaominglui <xiaominglui@gmail.com>
2020-03-23 21:04:45 +0800

    correct app title

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'A MaterialApp Title',
+      title: 'Word History Utilities for Google Dictionary Extension',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
