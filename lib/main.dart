8967156affa061d27ca7fbe8d31d0d4c645b9f46
xiaominglui <xiaominglui@gmail.com>
2020-07-03 06:49:34 +0800

    add minimal localization classes

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,70 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart' show SynchronousFuture;
 import 'package:word_history_utilities/home_page.dart';
 
-void main() => runApp(MyApp());
+class MinimalLocalizations {
+  MinimalLocalizations(this.locale);
+
+  final Locale locale;
+
+  static MinimalLocalizations of(BuildContext context) {
+    return Localizations.of<MinimalLocalizations>(context, MinimalLocalizations);
+  }
+
+  static Map<String, Map<String, String>> _localizedValues = {
+    'en': {
+      'title': 'Utilities for Word History of Google Dictionary Extension',
+    },
+    'zh': {
+      'title': '哈哈',
+    },
+  };
+
+  String get title {
+    return _localizedValues[locale.languageCode]['title'];
+  }
+}
+
+class MinimalLocalizationsDelegate extends LocalizationsDelegate<MinimalLocalizations> {
+  const MinimalLocalizationsDelegate();
+
+  @override
+  bool isSupported(Locale locale) => ['en', 'zh'].contains(locale.languageCode);
+
+  @override
+  Future<MinimalLocalizations> load(Locale locale) {
+    // Returning a SynchronousFuture here because an async "load" operation
+    // isn't needed to produce an instance of DemoLocalizations.
+    return SynchronousFuture<MinimalLocalizations>(MinimalLocalizations(locale));
+  }
+
+  @override
+  bool shouldReload(MinimalLocalizationsDelegate old) => false;
+}
+
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      onGenerateTitle: (BuildContext context) => MinimalLocalizations.of(context).title,
+      localizationsDelegates: [
+        const MinimalLocalizationsDelegate(),
+      ],
+      supportedLocales: [
+        const Locale('en', ''),
+        const Locale('es', ''),
+      ],
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new HomePage(),
+    );
+  }
+}
 
 class MyApp extends StatefulWidget {
   MyApp({Key key}) : super(key: key);
@@ -13,16 +74,4 @@ class MyApp extends StatefulWidget {
   _MyAppState createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Utilities for Word History of Google Dictionary Extension',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: new HomePage(),
-    );
-  }
-}
+void main() => runApp(MyApp());
